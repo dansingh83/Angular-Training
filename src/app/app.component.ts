d1a1101d47eae8f97168012c24518053b5afe363
Dan Singh <dan.singh@nihilent.com>
2025-11-10 11:32:19 +0530

    Latet code commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CatalogModule } from './catalog/catalog.module';
+ 
+
+import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 import { CommonModule } from '@angular/common';
-import { MembershipModule } from './membership/membership.module';
-import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CatalogModule, ShoppingCartModule, MembershipModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Transflower Store';
-  isList:boolean=false;
-  IsLogin:boolean=true;
+  title = 'estorespa';
 
-  reRoute(obj:boolean):void{
-    this.isList=obj;
+  loggedInStatus:boolean|undefined;
+ 
+  links:any=[];
+
+  constructor(){
+    this.links=["home","about","services", "protected"];
+  }
+  convertToBoolean(result:string):boolean{
+    let status=false;
+    if(result=="true"){
+      status=true;
+    }
+    return status;
+  }
+
+  ngOnInit() {
+    console.log("Router container component ngOnint is getting invoked");
+      let strStatus:string|null=localStorage.getItem("loggedInStatus");
+      console.log( " in routerocntainer strStatus ="+strStatus);
+      if(strStatus!=null){
+        this.loggedInStatus=true;
+        //this.convertToBoolean(strStatus);
+        console.log(this.loggedInStatus);
+      }
+
   }
 }
---
+++ src_1/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ListComponent } from './catalog/products/list/list.component';
+
+export const routes: Routes = [
+    { path: 'app-list', component: ListComponent }
+];
